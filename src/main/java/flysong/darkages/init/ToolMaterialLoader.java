485b749bc5d97b635fbc48d89906b23e527c31c3
theFlysong <song_of_the_fly@163.com>
2020-02-17 14:25:03 +0800

    2020-02-02

--- src/main/java/flysong/darkages/init/ToolMaterialLoader.java
+++ src/main/java/flysong/darkages/init/ToolMaterialLoader.java
@@ -1,4 +1,8 @@
 package flysong.darkages.init;
 
+import net.minecraft.item.Item.ToolMaterial;
+import net.minecraftforge.common.util.EnumHelper;
+
 public class ToolMaterialLoader {
+    public static final ToolMaterial ENERGY = EnumHelper.addToolMaterial("ENERGY",4,3072,11.0F,7.0F,14);
 }
